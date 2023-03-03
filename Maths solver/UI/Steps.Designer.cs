73005fe61edcc7a4c9f142ab938c41ecf7d95ce7
RandomPDF <107433904+RandomPDF@users.noreply.github.com>
2023-03-03 11:28:46 +0000

    FINAL EVER COMMIT????? Fixed a tonne more bugs

--- Maths solver/UI/Steps.Designer.cs
+++ Maths solver/UI/Steps.Designer.cs
@@ -28,59 +28,61 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.StepsBox = new System.Windows.Forms.RichTextBox();
-			this.ExitButton = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(12, 9);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(78, 29);
-			this.label1.TabIndex = 0;
-			this.label1.Text = "Steps";
-			// 
-			// StepsBox
-			// 
-			this.StepsBox.Cursor = System.Windows.Forms.Cursors.Arrow;
-			this.StepsBox.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.StepsBox.Location = new System.Drawing.Point(12, 41);
-			this.StepsBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.StepsBox.Name = "StepsBox";
-			this.StepsBox.ReadOnly = true;
-			this.StepsBox.Size = new System.Drawing.Size(1879, 930);
-			this.StepsBox.TabIndex = 1;
-			this.StepsBox.Text = "";
-			this.StepsBox.WordWrap = false;
-			// 
-			// ExitButton
-			// 
-			this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.ExitButton.Location = new System.Drawing.Point(12, 976);
-			this.ExitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-			this.ExitButton.Name = "ExitButton";
-			this.ExitButton.Size = new System.Drawing.Size(141, 55);
-			this.ExitButton.TabIndex = 2;
-			this.ExitButton.Text = "EXIT";
-			this.ExitButton.UseVisualStyleBackColor = true;
-			this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
-			// 
-			// Steps
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1284, 1006);
-			this.Controls.Add(this.ExitButton);
-			this.Controls.Add(this.StepsBox);
-			this.Controls.Add(this.label1);
-			this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-			this.Name = "Steps";
-			this.Text = "Steps";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StepsBox = new System.Windows.Forms.RichTextBox();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Steps";
+            // 
+            // StepsBox
+            // 
+            this.StepsBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.StepsBox.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.StepsBox.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StepsBox.Location = new System.Drawing.Point(9, 33);
+            this.StepsBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.StepsBox.Name = "StepsBox";
+            this.StepsBox.ReadOnly = true;
+            this.StepsBox.Size = new System.Drawing.Size(1410, 756);
+            this.StepsBox.TabIndex = 1;
+            this.StepsBox.Text = "";
+            this.StepsBox.WordWrap = false;
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ExitButton.Location = new System.Drawing.Point(9, 793);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(106, 45);
+            this.ExitButton.TabIndex = 2;
+            this.ExitButton.Text = "EXIT";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Steps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(963, 817);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.StepsBox);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Steps";
+            this.Text = "Steps";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
